43c1b87fa0b4e770d7fb6076ea77ea3e30729167
jonminton <nate.minton@gmail.com>
2019-08-09 16:02:24 +0100

    add init script - uk, enw, scot, usa

---
+++ Script.R
@@ -0,0 +1,255 @@
+
+# To do
+
+# 1. write ll function [done]
+# 2. get data from hmd 
+# 3. get differences for uk 
+# 4. calc ll for different extents of slowdown 
+
+pacman::p_load(
+  tidyverse,
+  HMDHFDplus
+)
+
+
+# Functions 
+get_ll <- function(x, mu, sig_sq){
+  n <- length(x)
+  
+   - ( n / 2 ) * log(sig_sq)  - (n/2) * log(2 * pi) - (1 / 2 * sig_sq) * sum((x - mu)^2)
+}
+
+calc_bayes_factors <- function(dta, before_period, after_period){
+  before_dta <- dta %>% 
+    filter(between(year, before_period[1], before_period[2]))
+  
+  if(length(after_period) == 1){
+    after_dta <- dta %>% 
+      filter(year >= after_period)
+  } else if (length(after_period) == 2){
+    after_dta <- dta %>% 
+      filter(between(year, after_period[1], after_period[2]))
+  }
+  
+  prev_params <- before_dta %>% 
+    summarise(
+      mu  = mean(ch_e0),
+      sig = sd(ch_e0) 
+    )
+  
+  ll_noslowdown <- get_ll(x = after_dta %>% pull(ch_e0), 
+                          mu = pull(prev_params, "mu"), 
+                          sig_sq = pull(prev_params, "sig") ^ 2
+                  )
+  
+  scenarios_df <- 
+    tibble(
+      perc = seq(from = 1, to =0, by = -0.01)
+    ) %>% 
+    mutate(
+      mu = pull(prev_params, "mu") * perc
+    ) %>% 
+    mutate(
+      ll = map_dbl(mu, ~get_ll(mu = ., x = after_dta %>% pull(ch_e0), 
+                               sig_sq = pull(prev_params, "sig") ^ 2))
+    ) %>% 
+    mutate(bayes_factor = exp(ll) /  exp(ll_noslowdown))
+  
+  
+  
+  return(scenarios_df)
+}
+
+getHMDcountries()
+
+dta <- HMDHFDplus::readHMDweb("GBR_NP", item = "E0per", username = userInput(), password = userInput())
+
+dta_tidy <- dta %>% 
+  as_tibble() %>% 
+  magrittr::set_names(tolower(names(.))) %>% 
+  gather(-year, key = "sex", value = "e0")
+
+# for now just want total 
+
+dta_total <- 
+  dta_tidy %>% 
+  filter(sex == "total") %>% 
+  arrange(year) %>% 
+  mutate(ch_e0 = e0 - lag(e0))
+
+
+# To start with: ch_e0 from 1990 to 2012
+
+prev_params <- 
+  dta_total %>% 
+  filter(between(year, 1991, 2012)) %>% 
+  summarise(
+    mu  = mean(ch_e0),
+    sig = sd(ch_e0) 
+  )
+
+new_x <- 
+  dta_total %>% 
+  filter(year > 2012) %>% 
+  pull(ch_e0)
+
+ll_noslowdown <- get_ll(x = new_x, mu = pull(prev_params, "mu"), sig_sq = pull(prev_params, "sig") ^ 2)
+
+# Create vector of various slowdown scenarios 
+
+pull(prev_params, "mu")
+
+scenarios_df <- 
+  tibble(
+    perc = seq(from = 1, to =0, by = -0.01)
+  ) %>% 
+  mutate(
+    mu = pull(prev_params, "mu") * perc
+  ) %>% 
+  mutate(
+    ll = map_dbl(mu, ~get_ll(mu = ., x = new_x, sig_sq = pull(prev_params, "sig") ^ 2))
+  ) %>% 
+  mutate(bayes_factor = exp(ll) /  exp(ll_noslowdown))
+
+
+ggplot(
+  scenarios_df, 
+  aes(y = bayes_factor, x = perc)
+) + 
+  geom_line() 
+
+# + 
+#   scale_y_log10(breaks = c(1/2, 1, 2, 5, 10), limits = c(0.5, 10))
+
+# This has the shape I was expecting, but the magnitude was much less than I was expecting 
+# With the log scale it just looks like a flat line 
+
+
+# Let's build a function for automating some of the above 
+
+# pre_years, post_years 
+
+
+# How about? 
+
+# Adding last available year? 
+
+
+# total since 1950? 
+# by gender? 
+
+tibble(
+  period = c("2013", "2013-14", "2013-15", "2013-16", "2013-17"),
+  after_end = 2013:2017
+) %>% 
+  mutate(
+    bayes_df = map(
+      after_end, ~calc_bayes_factors(after_period = c(2013, .), before_period = c(1990, 2012), dta = dta_total)
+    )
+  ) %>% 
+  select(-after_end) %>% 
+  unnest() %>% 
+  ggplot(aes(x = perc, y = bayes_factor, group = period, colour = period)) + 
+  geom_line()
+
+# by gender
+
+dta_tidy %>% 
+  group_by(sex) %>% 
+  arrange(year) %>% 
+  mutate(ch_e0 = e0 - lag(e0)) %>% 
+  nest() %>% 
+  crossing(after_end = 2013:2016) %>% 
+  mutate(
+    bayes_df = map2(
+      after_end, data, ~calc_bayes_factors(after_period = c(2013, .x), before_period = c(1990, 2012), dta = .y)
+    )
+  ) %>%
+  select(sex, after_end, bayes_df) %>% 
+  unnest() %>% 
+  ggplot(aes(x = perc, y = bayes_factor, group = after_end, colour = factor(after_end))) + 
+  geom_line() +
+  facet_wrap(~sex) + 
+  geom_hline(yintercept = 1)
+
+
+# So, the evidence is still at the 'anecdotal' level, but growing
+
+# USA - since 1999? 
+
+dta_usa <- HMDHFDplus::readHMDweb("USA", item = "E0per", username = userInput(), password = userInput()) 
+
+dta_usa %>% 
+  as_tibble() %>% 
+  magrittr::set_names(tolower(names(.))) %>% 
+  gather(-year, key = "sex", value = "e0") %>% 
+  group_by(sex) %>% 
+  arrange(year) %>% 
+  mutate(ch_e0 = e0 - lag(e0)) %>% 
+  nest() %>% 
+  crossing(after_end = 1999:2017) %>% 
+  mutate(
+    bayes_df = map2(
+      after_end, data, ~calc_bayes_factors(after_period = c(1999, .x), before_period = c(1960, 1998), dta = .y)
+    )
+  ) %>%
+  select(sex, after_end, bayes_df) %>% 
+  unnest() %>% 
+  ggplot(aes(x = perc, y = bayes_factor, group = after_end, colour = after_end)) + 
+  geom_line() +
+  facet_wrap(~sex) + 
+  geom_hline(yintercept = 1) +
+  scale_y_continuous(limits = c(1, 1.002))
+
+# Now England/Wales alone 
+
+dta_enw <- HMDHFDplus::readHMDweb("GBRTENW", item = "E0per", username = userInput(), password = userInput()) 
+
+dta_enw %>% 
+  as_tibble() %>% 
+  magrittr::set_names(tolower(names(.))) %>% 
+  gather(-year, key = "sex", value = "e0") %>% 
+  group_by(sex) %>% 
+  arrange(year) %>% 
+  mutate(ch_e0 = e0 - lag(e0)) %>% 
+  nest() %>% 
+  crossing(after_end = 2012:2017) %>% 
+  mutate(
+    bayes_df = map2(
+      after_end, data, ~calc_bayes_factors(after_period = c(2012, .x), before_period = c(1990, 2011), dta = .y)
+    )
+  ) %>%
+  select(sex, after_end, bayes_df) %>% 
+  unnest() %>% 
+  ggplot(aes(x = perc, y = bayes_factor, group = after_end, colour = after_end)) + 
+  geom_line() +
+  facet_wrap(~sex) + 
+  geom_hline(yintercept = 1) +
+  scale_y_continuous(limits = c(1, 1.005))
+
+dta_sco <- HMDHFDplus::readHMDweb("GBR_SCO", item = "E0per", username = userInput(), password = userInput()) 
+
+dta_sco %>% 
+  as_tibble() %>% 
+  magrittr::set_names(tolower(names(.))) %>% 
+  gather(-year, key = "sex", value = "e0") %>% 
+  group_by(sex) %>% 
+  arrange(year) %>% 
+  mutate(ch_e0 = e0 - lag(e0)) %>% 
+  nest() %>% 
+  crossing(after_end = 2012:2017) %>% 
+  mutate(
+    bayes_df = map2(
+      after_end, data, ~calc_bayes_factors(after_period = c(2012, .x), before_period = c(1990, 2011), dta = .y)
+    )
+  ) %>%
+  select(sex, after_end, bayes_df) %>% 
+  unnest() %>% 
+  ggplot(aes(x = perc, y = bayes_factor, group = after_end, colour = after_end)) + 
+  geom_line() +
+  facet_wrap(~sex) + 
+  geom_hline(yintercept = 1) +
+  scale_y_continuous(limits = c(1, 1.005))
+
+
+
