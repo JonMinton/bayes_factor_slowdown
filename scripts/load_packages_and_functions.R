74467341c1e17d245efd677dacb2cf08dad77be9
jonminton <nate.minton@gmail.com>
2019-08-19 17:07:10 +0100

    create devils advocate markdown

--- scripts/load_packages_and_functions.R
+++ scripts/load_packages_and_functions.R
@@ -59,3 +59,48 @@ calc_bayes_factors <- function(dta, before_period, after_period, outcome_var, pe
 }
 
 
+calc_devils_bayes_factors <- function(dta, full_period, drop_period, outcome_var, perc_range = seq(from = 1, to = 2.00, by = 0.01)){
+
+  full_dta <- 
+    dta %>% 
+    filter(between(year, full_period[1], full_period[2]))
+  
+  drop_dta <- 
+    dta %>% 
+    filter(between(year, drop_period[1], drop_period[2]))
+
+  fulldrop_dta <- 
+    full_dta %>% 
+    anti_join(drop_dta)
+  
+
+  prev_params <- fulldrop_dta %>% 
+    summarise(
+      mu  = mean({{ outcome_var }}),
+      sig = sd({{ outcome_var }}) 
+    )
+  
+  ll_noslowdown <- get_ll(x = drop_dta %>% pull({{ outcome_var }}), 
+                          mu = pull(prev_params, "mu"), 
+                          sig_sq = pull(prev_params, "sig") ^ 2
+  )
+  
+  scenarios_df <- 
+    tibble(
+      perc = perc_range
+    ) %>% 
+    mutate(
+      mu = pull(prev_params, "mu") * perc
+    ) %>% 
+    mutate(
+      ll = map_dbl(mu, ~get_ll(mu = ., x = drop_dta %>% pull({{ outcome_var }}), 
+                               sig_sq = pull(prev_params, "sig") ^ 2))
+    ) %>% 
+    mutate(bayes_factor = exp(ll) /  exp(ll_noslowdown))
+  
+  
+  
+  return(scenarios_df)
+}
+
+
