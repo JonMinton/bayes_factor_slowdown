72d17be5c90e46168d5add3d3879e18590d9ada2
Jon Minton <jon.will.minton@gmail.com>
2019-09-26 14:17:03 +0100

    update for new year (but can't access hmd)

--- scripts/create_e0_uk.R
+++ scripts/create_e0_uk.R
@@ -4,16 +4,39 @@
 
 # getHMDcountries()
 
-dta_uk <- HMDHFDplus::readHMDweb("GBR_NP", item = "E0per", username = userInput(), password = userInput())
+dta_uk <- HMDHFDplus::readHMDweb(CNTRY = "GBR_NP", item = "E0per", username = userInput(), password = userInput())
 
 # Now 201&
 # link here: https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/lifeexpectancies/adhocs/0091452017singleyearnationallifetables
 # https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/lifeexpectancies/adhocs/0091452017singleyearnationallifetables/2017singleyearlifetables.xls
 
+# Life expectancy for UK, 2017 and 2018
+
+# https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/lifeexpectancies/datasets/singleyearlifetablesuk1980to2018
+
+# Copied to data/singleyearlifetablesunitedkingdom1.xls
+
+# The values 
+
+# 2017:
+
 # Saved to data 
 # The only items needed are 
-# e0 male   : 77.13
-# e0 female : 81.16
+# e0 male   : 79.28
+# e0 female : 82.98
+
+e0_male_2017   <- 79.28
+e0_female_2017 <- 82.98 
+
+# 2018
+# Saved to data 
+# The only items needed are 
+# e0 male   : 79.24
+# e0 female : 82.93
+
+e0_male_2018   <- 79.24
+e0_female_2018 <- 82.93 
+
 
 # Population from here: 
 
@@ -27,7 +50,21 @@ dta_uk <- HMDHFDplus::readHMDweb("GBR_NP", item = "E0per", username = userInput(
 # male: 31.9 million
 # female: 32.82 million
 
-e0_total <- (79.23 * 31.9 + 82.98 * 32.82) / (31.9 + 32.82)
+p_male_2017 <- 31.9
+p_female_2017 <- 32.82
+
+e0_total_2017 <- (e0_male_2017 * p_male_2017 + e0_female_2017 * p_female_2017) / (p_male_2017 + p_female_2017)
+
+# and for 2018
+
+#https://www.wolframalpha.com/input/?i=UK+population+2018+by+sex
+# male: 32.96 million
+# female: 33.74 million
+
+p_male_2018 <- 32.96
+p_female_2018 <- 33.74
+
+e0_total_2018 <- (e0_male_2018 * p_male_2018 + e0_female_2018 * p_female_2018) / (p_male_2018 + p_female_2018)
 
 
 dta_tidy <- dta_uk %>% 
@@ -40,10 +77,14 @@ dta_tidy <-
   bind_rows(
     tribble(
       ~year, ~sex, ~e0,
-      2017, "female", 82.98,
-      2017, "male"  , 79.23, 
-      2017, "total" , e0_total 
+      2017, "female", e0_female_2017,
+      2017, "male"  , e0_male_2017, 
+      2017, "total" , e0_total_2017,
+      2018, "female", e0_female_2018,
+      2018, "male"  , e0_male_2018, 
+      2018, "total" , e0_total_2018 
+      
     )
   )
 
-
+write_csv(dta_tidy, "data/e0_uk.csv")
